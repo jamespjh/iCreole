9d983710271a446075e8ed644816b656d04dc855
James Hetherington <jamespjh@googlemail.com>
2010-12-09 10:30:49 +0000

    Initial checkin

---
+++ MyDocument.h
@@ -0,0 +1,15 @@
+//
+//  MyDocument.h
+//  iCreole
+//
+//  Created by James Hetherington on 09/12/2010.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+
+#import <Cocoa/Cocoa.h>
+
+@interface MyDocument : NSDocument
+{
+}
+@end
---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  iCreole
+//
+//  Created by James Hetherington on 09/12/2010.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **) argv);
+}
